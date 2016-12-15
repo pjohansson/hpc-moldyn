4b6fb35a6c5b3edbfbb86b0e158734f17c37510c
Petter Johansson <pettjoha@kth.se>
2016-12-15 20:29:32 +0100

    Move test utility declarations to header

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -2,44 +2,6 @@
 #include <sstream>
 #include <string>
 
-//#define ASSERT(cond, err_string) if (!cond) { print_test_fail(err_string, __func__, __LINE__); }
-
-// Compare to input objects and see that they are equivalent. They may
-// be of different types, in which case a static_cast must be possible
-// to perform.
-#define ASSERT_EQ(a, b, err_string) assert_eq(a, b, err_string, __func__, __LINE__);
-
-// Compare two input arrays and assert that they are identical.
-// The objects must be of identical types implementing size(), cbegin()
-// and cend() methods for iteration over the values. Their size and
-// values are both controlled.
-#define ASSERT_EQ_VEC(a, b, err_string) assert_eq_vec(a, b, err_string, __func__, __LINE__);
-
-// Macro which adds a main function which runs the input functions.
-#define RUN_TESTS(...) \
-    int main(int argc, char* argv[]) { \
-        std::cout << "Running tests in '" << __FILE__ << "'.\n"; \
-        __VA_ARGS__ \
-        if (num_failed == 0) { \
-            cout << "All tests passed.\n\n"; \
-        } \
-        else if (num_failed == 1) { \
-            cout << num_failed << " test failed.\n\n"; \
-        } \
-        else { \
-            cout << num_failed << " tests failed.\n\n"; \
-        } \
-        return num_failed; \
-    }
-
-// Macro which wraps a test function with a void declaration
-// and updates the test status afterwards.
-#define ADD_TEST(name, ...) \
-    void name() { \
-        __VA_ARGS__ \
-        finalize_test(); \
-    }
-
 int num_failed = 0;
 bool test_failed = false;
 
