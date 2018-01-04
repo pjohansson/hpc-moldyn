a8bf5fdd82228bb6953a1dbaf86c41d3e9a7dfcd
Petter Johansson <pettjoha@kth.se>
2018-01-04 14:07:25 +0100

    Add `params.h`

---
+++ src/params.h
@@ -0,0 +1,42 @@
+#include <cmath>
+
+#include "conf.h"
+
+#ifndef SIMULATION_PARAMETERS_H
+#define SIMULATION_PARAMETERS_H
+
+struct ForceField {
+    constexpr ForceField(const real epsilon,
+                         const real sigma,
+                         const real rcut,
+                         const real mass)
+    :epsilon { epsilon },
+     sigma { sigma },
+     mass { mass },
+     c6 { 24.0 * epsilon * std::pow(sigma, 6) },
+     c12 { 48.0 * epsilon * std::pow(sigma, 12) },
+     rcut { rcut },
+     rcut2 { std::pow(rcut, 2) }
+    {}
+
+    real epsilon,
+         sigma,
+         mass,
+         c6,
+         c12,
+         rcut,
+         rcut2;
+};
+
+struct Options {
+    constexpr Options(const double dt)
+    :dt { dt },
+     dt2 { dt * dt }
+    {}
+
+    double dt, dt2;
+};
+
+constexpr ForceField DefaultFF = ForceField(1.0, 1.0, 1.0, 1.0);
+
+#endif // SIMULATION_PARAMETERS_H
